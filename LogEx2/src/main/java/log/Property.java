f9007966616b61cb59a4c9b9bc23f90b0ccb2cc6
tanliwei <tanliwei@users.noreply.github.com>
2017-01-03 10:41:43 +0800

    Update Property.java

--- LogEx2/src/main/java/log/Property.java
+++ LogEx2/src/main/java/log/Property.java
@@ -4,8 +4,6 @@ package log;
  * TODO 配置文件
  * @author tanlw
  * @version $ID: Property.java, v 0.1 2016-12-10 16:49
- * @ Email: tanlw@fenqi.im
- * @ Copyright (C), 上海秦苍信息科技有限公司
  */
 public class Property {
     //encoding
