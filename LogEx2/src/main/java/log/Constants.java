ea0e7bda5c7605030ed98b937ed914d17fc211b3
tanliwei <tanliwei@users.noreply.github.com>
2017-01-03 10:40:31 +0800

    Update Constants.java

--- LogEx2/src/main/java/log/Constants.java
+++ LogEx2/src/main/java/log/Constants.java
@@ -3,8 +3,6 @@ package log;
 /**
  * @author tanlw
  * @version $ID: Constants.java, v 0.1 2016-12-06 19:03
- * @ Email: tanlw@fenqi.im
- * @ Copyright (C), 上海秦苍信息科技有限公司
  */
 public class Constants {
     public static final String LINE_SEPARATOR = "\r\n";
